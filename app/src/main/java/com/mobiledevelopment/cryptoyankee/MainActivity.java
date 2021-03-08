a1c70a9e555993656425f7de3178a44dcd40ad40
yasharzb <yasharzb@ce.sharif.edu>
2021-03-09 00:20:04 +0330

    Update MainActivity due to recent major changes.

--- app/src/main/java/com/mobiledevelopment/cryptoyankee/MainActivity.java
+++ app/src/main/java/com/mobiledevelopment/cryptoyankee/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mobiledevelopment.cryptoyankee;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,80 +9,109 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.mobiledevelopment.cryptoyankee.adaptars.CoinAdapter;
-import com.mobiledevelopment.cryptoyankee.models.CoinModel;
+import com.mobiledevelopment.cryptoyankee.adapter.CoinAdapter;
+import com.mobiledevelopment.cryptoyankee.db.dao.CoinRepository;
+import com.mobiledevelopment.cryptoyankee.db.entity.Coin;
+import com.mobiledevelopment.cryptoyankee.model.CoinDTO;
+import com.mobiledevelopment.cryptoyankee.util.CoinModelConverter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
-    CoinAdapter adapter;
-    RecyclerView recyclerView;
-    SwipeRefreshLayout swipeRefreshLayout;
-    List<CoinModel> items = new ArrayList<>();
+    private CoinAdapter coinAdapter;
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private CoinRepository coinRepository;
+    private CoinModelConverter coinModelConverter;
+//    private List<CoinDTO> coins = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coin_main);
+        Log.d("Chq", "Main");
         Objects.requireNonNull(getSupportActionBar()).setTitle("Price Indication");
         swipeRefreshLayout = findViewById(R.id.rootLayout);
-        swipeRefreshLayout.post(() -> loadFirst10Coin(0));
+        swipeRefreshLayout.post(this::loadTenCoins);
         swipeRefreshLayout.setOnRefreshListener(() -> {
-            items.clear();
             Toast.makeText(MainActivity.this, "Please Wait till loading is complete.", Toast.LENGTH_SHORT).show();
             // loading bar logic
-            loadFirst10Coin(0);
+            loadTenCoins();
         });
-        recyclerView = findViewById(R.id.coinList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        setupAdapter();
+        setupBeans();
     }
 
 
+/*
     private void loadFirst10Coin(int index) {
         swipeRefreshLayout.setRefreshing(true);
-        CoinModel testItem = new CoinModel("1", "bitcoin", "$", "50000", "3", "12", "20");
+        CoinDTO testItem = new CoinDTO("1", "bitcoin", "$", "50000", "3", "12", "20");
 
         // fetch items
 
         runOnUiThread(() -> {
-            List<CoinModel> newItems = Arrays.asList(testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem);
-            adapter.updateData(newItems);
-
+            List<CoinDTO> newItems = Arrays.asList(testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem, testItem);
+            coinAdapter.setCoins(newItems);
+            coinAdapter.notifyDataSetChanged();
         });
 
         if (swipeRefreshLayout.isRefreshing())
             swipeRefreshLayout.setRefreshing(false);
     }
+*/
 
-    private void setupAdapter() {
-        adapter = new CoinAdapter(recyclerView, MainActivity.this, items);
-        recyclerView.setAdapter(adapter);
+    private void setupBeans() {
+        coinModelConverter = CoinModelConverter.getInstance();
+        //TODO add some sample coins to register some data in DB
+        List<Coin> coins = new ArrayList<>();
+        coinRepository.putCoins(coins);
+        recyclerView = findViewById(R.id.coinList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        coinRepository = CoinRepository.getInstance(getBaseContext());
+        coinAdapter = CoinAdapter.getInstance();
+        recyclerView.setAdapter(coinAdapter);
+/*
         adapter.setiLoadMore(() -> {
-            if (items.size() <= 1000) //Max Size is 1000 coins
+            if (coins.size() <= 1000) //Max Size is 1000 coins
             {
-                loadNext10coin(items.size());
+                loadNext10coin(coins.size());
             } else {
                 Toast.makeText(MainActivity.this, "Max items is 1000", Toast.LENGTH_SHORT).show();
             }
         });
+*/
     }
 
+/*
     private void loadNext10coin(int index) {
-        List<CoinModel> newItems = Collections.singletonList(new CoinModel("123", "bitcoin", "$", "50000", "3", "12", "20"));
+        List<CoinDTO> newItems = Collections.singletonList(new CoinDTO("123", "bitcoin", "$", "50000", "3", "12", "20"));
 
         // fetch new items
 
         runOnUiThread(() -> {
-            items.addAll(newItems);
-            adapter.setLoaded();
-            adapter.updateData(items);
+            coins.addAll(newItems);
+//            adapter.setLoaded();
+            coinAdapter.setCoins(coins);
             swipeRefreshLayout.setRefreshing(false);
         });
+    }
+*/
+
+    private void loadTenCoins() {
+        List<Coin> coins = coinRepository.getTenCoins();
+        List<CoinDTO> coinDTOS = new ArrayList<>();
+        coins.forEach(coin -> coinDTOS.add(coinModelConverter.getCoinDTO(coin)));
+        coinAdapter.setCoins(coinDTOS);
+        coinAdapter.notifyDataSetChanged();
+    }
+
+    private void storeCoins() {
+        List<CoinDTO> coinDTOS = new ArrayList<>();
+        List<Coin> coins = new ArrayList<>();
+        coinDTOS.forEach(coinDTO -> coins.add(coinModelConverter.getCoinEntity(coinDTO)));
+        coinRepository.updateCoins(coins);
     }
 }
