9ce42d508254ec8b3bca932f660ffa97ea834e72
yasharzb <yasharzb@ce.sharif.edu>
2021-03-11 01:08:58 +0330

    Add ApiConnectivityException as an alert to failed network connectivity

---
+++ app/src/main/java/com/mobiledevelopment/cryptoyankee/model/exception/ApiConnectivityException.java
@@ -0,0 +1,7 @@
+package com.mobiledevelopment.cryptoyankee.model.exception;
+
+public class ApiConnectivityException extends RuntimeException{
+    public ApiConnectivityException() {
+        super();
+    }
+}
